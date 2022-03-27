4fd1843643465f085c0c6bcde549ac50c70dcc79
Awalone01 <sashaneiv01@yandex.ru>
2022-03-27 18:40:30 +0300

    Commit 1 HW14

--- src/com/company/list/impl/IntegerListImpl.java
+++ src/com/company/list/impl/IntegerListImpl.java
@@ -1,75 +1,196 @@
 package com.company.list.impl;
 
+import com.company.exception.InputNullException;
+import com.company.exception.NotFoundException;
+import com.company.exception.WrongIndexException;
 import com.company.list.IntegerList;
 
+import java.util.Arrays;
+
 public class IntegerListImpl implements IntegerList {
+
+    private Integer[] storage;
+    private static final int defaultSize = 10;
+    private int size;
+
+    public IntegerListImpl() {
+        this.storage = new Integer[defaultSize];
+    }
+
     @Override
     public Integer add(Integer item) {
-        return null;
+        checkArrayIfNull(item);
+        if (size == storage.length) {
+            increaseArray();
+        }
+        storage[size++] = item;
+        return item;
+    }
+
+    private void increaseArray() {
+        int newSize = storage.length * 2;
+        storage = Arrays.copyOf(storage, newSize);
+    }
+
+    private void checkArrayIfNull(Integer item) {
+        if (item == null) {
+            throw new InputNullException("Входящее значение является пустым");
+        }
     }
 
     @Override
     public Integer add(int index, Integer item) {
-        return null;
+        checkArrayIfNull(item);
+        if (index < 0 || index > size) {
+            throw new WrongIndexException("Введён неправильный индекс");
+        }
+        if (size == storage.length) {
+            increaseArray();
+        }
+        System.arraycopy(storage, index, storage, index + 1, size - index);
+        storage[index] = item;
+        size++;
+        return item;
     }
 
     @Override
     public Integer set(int index, Integer item) {
-        return null;
+        checkArrayIfNull(item);
+        if (index < 0 || index > size) {
+            throw new WrongIndexException("Введён неправильный индекс");
+        }
+        Integer previousElement = storage[index];
+        storage[index] = item;
+        return previousElement;
     }
 
     @Override
     public Integer remove(Integer item) {
-        return null;
+        checkArrayIfNull(item);
+        int removeElement = indexOf(item);
+        if (removeElement == -1) {
+            throw new NotFoundException("Запрашиваемый элемент не найден");
+        }
+        System.arraycopy(storage, removeElement, storage, removeElement,
+                size - removeElement);
+        size--;
+        return item;
     }
 
     @Override
     public Integer remove(int index) {
-        return null;
+        if (index < 0 || index > size) {
+            throw new WrongIndexException("Введён неправильный индекс");
+        }
+        Integer removeElementByIndex = storage[index];
+        System.arraycopy(storage, index + 1, storage, index,
+                size - index);
+        size--;
+        return removeElementByIndex;
     }
 
     @Override
     public boolean contains(Integer item) {
-        return false;
+        checkArrayIfNull(item);
+        selectionSort(storage);
+        return binarySearch(storage, item) != -1;
+    }
+
+    private Integer binarySearch(Integer[] array, Integer item) {
+        int min  = 0;
+        int max = size - 1;
+        while (min <= max) {
+            int average = (min+max)/2;
+
+            if (item.equals(array[average])) {
+                return average;
+            }
+            if (item < array[average]) {
+                max = average - 1;
+            } else {
+                min = average + 1;
+            }
+        }
+        return -1;
+    }
+
+    private static void selectionSort(Integer[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            swapElements(arr, i, min);
+        }
+    }
+
+    private static void swapElements(Integer[] firstArray, int j, int i) {
+        int tmp = firstArray[i];
+        firstArray[i] = firstArray[j];
+        firstArray[j] = tmp;
     }
 
     @Override
     public int indexOf(Integer item) {
-        return 0;
+        for (int i = 0; i < size - 1; i++) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Integer item) {
-        return 0;
+        checkArrayIfNull(item);
+        for (int i = size - 1; i >= 0; i--) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public Integer get(int index) {
-        return null;
+        if (index < 0 || index > size) {
+            throw new WrongIndexException("Введён неправильный индекс");
+        }
+        return storage[index];
     }
 
     @Override
     public boolean equals(IntegerList otherList) {
-        return false;
+        if (otherList == null || size != otherList.size()) {
+            return false;
+        }
+        for (int i = 0; i < size - 1; i++) {
+            get(i);
+            otherList.get(i);
+        }
+        return true;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
-
+        storage = new Integer[defaultSize];
+        size = 0;
     }
 
     @Override
     public Integer[] toArray() {
-        return new Integer[0];
+        return Arrays.copyOf(storage, size);
     }
 }
